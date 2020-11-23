c4e0831243168dfd0b7e30a48b50c7ae02ba832d
Bartek Domanski <bdomanski57@gmail.com>
2020-11-23 20:26:04 +0100

    user layer added

---
+++ src/main/java/com/travel_agency/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.travel_agency.mapper;
+
+import com.travel_agency.dto.UserDto;
+import com.travel_agency.model.user.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    static UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto userDto(User user);
+    User dtoToUser(UserDto userDto);
+
+}
