b9cfd0a4819265e17a13823e34a4792f868a8501
bartlomiej.domanski <bdomanski57@gmail.com>
2020-12-15 19:00:58 +0100

    destination Service and Controller changes

--- src/main/java/com/travel_agency/controller/DestinationController.java
+++ src/main/java/com/travel_agency/controller/DestinationController.java
@@ -1,9 +1,52 @@
 package com.travel_agency.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.travel_agency.dto.DestinationDTO;
+import com.travel_agency.dto.TripDTO;
+import com.travel_agency.service.DestinationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("")
 public class DestinationController {
+
+    DestinationService destinationService;
+
+    @Autowired
+    public DestinationController(DestinationService destinationService) {
+        this.destinationService = destinationService;
+    }
+
+    @ResponseBody
+    @RequestMapping("destinations")
+    public List<DestinationDTO> getAllDestinations(){
+        return destinationService.getAllDestinations();
+    }
+
+    @ResponseBody
+    @RequestMapping("/destination/{id}")
+    public DestinationDTO getDestinationById(@PathVariable Long id){
+        return destinationService.getDestinationById(id);
+    }
+
+    @GetMapping("/deleteDestination")
+    public void deleteDestination(@RequestParam Long id){
+        destinationService.deleteDestinationById(id);
+    }
+
+    @PostMapping("/editDestination")
+    public void editDestionation(@RequestParam Long id, @RequestBody DestinationDTO destinationDTO){
+        destinationService.editAndSaveDestination(id, destinationDTO);
+    }
+
+    @GetMapping("/destination/{id}/trips")
+    public List<TripDTO> getTripsForDestination(@PathVariable Long id){
+        return destinationService.getTripsForDestination(id);
+    }
+
+
+
+
 }
---
+++ src/main/java/com/travel_agency/mapper/DestinationMapper.java
@@ -0,0 +1,17 @@
+package com.travel_agency.mapper;
+
+import com.travel_agency.dto.DestinationDTO;
+import com.travel_agency.dto.HotelDTO;
+import com.travel_agency.model.destination.Destination;
+import com.travel_agency.model.hotel.Hotel;
+import org.mapstruct.factory.Mappers;
+
+public interface DestinationMapper {
+
+    static DestinationMapper INSTANCE = Mappers.getMapper(DestinationMapper.class);
+
+    DestinationDTO destinationDTO(Destination destination);
+    Destination dtoToDestination(DestinationDTO destinationDTO);
+
+
+}
