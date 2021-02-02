522e1ef45cbcd5cab59fdbe45c090561cb3342bc
Bartek Domanski <bdomanski57@gmail.com>
2021-02-02 18:05:18 +0100

    changes

--- src/main/java/com/travel_agency/controller/TripController.java
+++ src/main/java/com/travel_agency/controller/TripController.java
@@ -1,17 +1,22 @@
 package com.travel_agency.controller;
 
+import com.travel_agency.dto.HotelDTO;
 import com.travel_agency.dto.TripDTO;
+import com.travel_agency.model.trip.Trip;
+import com.travel_agency.model.trip.TripAlimentationEnum;
+import com.travel_agency.model.trip.TripStatusEnum;
 import com.travel_agency.model.trip.TripTypeEnum;
 import com.travel_agency.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class TripController {
@@ -28,16 +33,93 @@ public class TripController {
     @ResponseBody
     @GetMapping("/trips")
     public List<TripDTO> getAllTrips() {
-//        List<TripDTO> trips = tripService.getAllTrips();
-//        model.addAttribute("tripsList", trips);
-//        return "trips";
+
         return tripService.getAllTrips();
     }
 
-    @ResponseBody
-    @GetMapping(value = "/trips/{id}", produces = "application/json")
-    TripDTO getTripById(@PathVariable("id") Long id) {
-        return tripService.getTripById(id);
+
+    @GetMapping("/tripsByType")
+    public String getTripByType(@RequestParam TripTypeEnum type, Model model) {
+        List<TripDTO> tripsByTripType = tripService.getTripByTripType(type);
+        model.addAttribute("tripsByTypes", tripsByTripType);
+        model.addAttribute("tripType", type.name());
+        tripService.setAttributesForMainPage(model);
+        return "tripsByType";
+
+    }
+
+
+    @GetMapping(value = "/tripsByAlimentation/{alimentation}")
+    public String getTripByAlimentation(@PathVariable String alimentation, Model model) {
+        TripAlimentationEnum alimentationEnum = TripAlimentationEnum.valueOf(alimentation);
+        List<TripDTO> tripsByTripType = tripService.getTripByAlimentation(alimentationEnum);
+
+        model.addAttribute("tripsByTypes", tripsByTripType);
+        model.addAttribute("tripType", alimentationEnum.name());
+        tripService.setAttributesForMainPage(model);
+
+        return "tripsByType";
+
+    }
+
+    @PostMapping(value = "/tripSearch")
+    public String getTripsFromSearchBox(@RequestParam String start, @RequestParam String end,
+                                        @RequestParam String continent, Model model) {
+        LocalDateTime startDate = LocalDateTime.parse(start);
+        LocalDateTime endDate = LocalDateTime.parse(end);
+
+        List<TripDTO> tripBySearchBox = tripService.getTripBySearchBox(startDate, endDate, continent);
+        model.addAttribute("tripsSearched", tripBySearchBox);
+        tripService.setAttributesForMainPage(model);
+
+        return "tripsSearched";
+    }
+
+    @GetMapping("/newTrip")
+    public String createNewTrip(Model model) {
+        List<TripTypeEnum> tripTypeEnums = Arrays.asList(TripTypeEnum.values());
+        List<TripAlimentationEnum> tripAlimentationEnums = Arrays.asList(TripAlimentationEnum.values());
+        Trip trip = new Trip();
+
+        model.addAttribute("tripTypes", tripTypeEnums);
+        model.addAttribute("tripAlimentations", tripAlimentationEnums);
+
+        return "addTrip";
+
+    }
+
+    @PostMapping("/newTrip")
+    public String addNewTrip(@RequestParam String start, @RequestParam String end, @RequestParam String adultPrice, @RequestParam String childPrice,
+                             @RequestParam TripTypeEnum type, @RequestParam TripAlimentationEnum alimentation, @RequestParam String description,
+                             @RequestParam Integer limit, @RequestParam String continent, @RequestParam String country,
+                             @RequestParam String city, @RequestParam String airport, @RequestParam String url, Model model) {
+
+        LocalDateTime startTime = LocalDateTime.parse(start);
+        LocalDateTime endTime = LocalDateTime.parse(end);
+        BigDecimal adultPriceTag = new BigDecimal(adultPrice);
+        BigDecimal childPriceTag = new BigDecimal(childPrice);
+
+        tripService.createAndSaveNewTrip(startTime, endTime, adultPriceTag,
+                childPriceTag, type, alimentation, description,
+                limit, url, continent, country, city, airport);
+
+
+        model.addAttribute("message", "Pomyślnie utworzono nową wycieczkę");
+        tripService.setAttributesForMainPage(model);
+
+        return "message";
+    }
+
+
+
+    @GetMapping(value = "/trip/{id}")
+    public String getTripById(@PathVariable("id") Long id, Model model) {
+
+        TripDTO tripById = tripService.getTripById(id);
+        List<HotelDTO> hotelsForTrip = tripService.getHotelsForTripById(id);
+        model.addAttribute("trip", tripById);
+        model.addAttribute("hotelsForTrip", hotelsForTrip);
+        return "tripDetails";
     }
 
     @ResponseBody
@@ -55,18 +137,9 @@ public class TripController {
     }
 
     @ResponseBody
-    @GetMapping(value="/counter/trip/{id}", produces = "application/json")
+    @GetMapping(value = "/counter/trip/{id}", produces = "application/json")
     Integer getCounterForTripWithId(@PathVariable Long id) {
         return tripService.getCounterForTripWithId(id);
-    }
-
-    @GetMapping("/tripsByType")
-    public String getTripByType(@RequestParam TripTypeEnum type, Model model) {
-        List<TripDTO> tripsByTripType = tripService.getTripByTripType(type);
-        model.addAttribute("tripsByTypes", tripsByTripType);
-        model.addAttribute("tripType", type.name());
-        tripService.setAttributesForMainPage(model);
-        return "tripsByType";
     }
 
 }
